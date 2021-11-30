d9c0d670c84d6ae092b27501c64ebabfe0f0469e
mauroarantes <94620494+mauroarantes@users.noreply.github.com>
2021-11-30 12:27:32 +0000

    Add files via upload

---
+++ NumberPalindrome1.playground/Contents.swift
@@ -0,0 +1,38 @@
+import UIKit
+
+func reverse(_ y: Int) -> Int {
+    
+    var i = 1
+    var sign = 1
+    var ans = 0
+    var x = abs(y)
+    if x < 0 {
+       sign = -1
+    }
+    while x > 0 {
+//        print("i = ", i)
+        ans *= 10
+        ans += x%10
+        x /= 10
+        i += 1
+    }
+//    if y == ans*sign {
+//        return i
+//    } else {
+//        reverse(ans*sign+y)
+//        print("Ans = ", ans*sign+y)
+//    }
+    return ans*sign
+}
+
+var j = 0
+var num = 99887766
+var rev = reverse(num)
+var sum = num + rev
+//print("Rev1 = ", rev)
+//print("Sum = ", sum)
+while sum != reverse(sum) {
+    j += 1
+    print("Palindrome = ", sum + reverse(sum), ", after ", j, " iteractions.")
+    sum += reverse(sum)
+}
