e5f39700a5c29b8586cc313758f51dc2e2fb0ffe
mauroarantes <94620494+mauroarantes@users.noreply.github.com>
2021-12-01 13:04:21 +0000

    30.11.2021

---
+++ Tuesday2/Tuesday2Tests/Tuesday2Tests.swift
@@ -0,0 +1,33 @@
+//
+//  Tuesday2Tests.swift
+//  Tuesday2Tests
+//
+//  Created by Mauro Arantes on 30/11/2021.
+//
+
+import XCTest
+@testable import Tuesday2
+
+class Tuesday2Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
